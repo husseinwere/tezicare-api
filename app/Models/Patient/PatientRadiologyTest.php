53922ceb29b29b413ce3bb83b7e19491ae5304b6
husseinwere <hussein.were@yahoo.com>
2023-08-26 08:43:24 +0300

    patient radiology tests

---
+++ app/Models/Patient/PatientRadiologyTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Patient;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PatientRadiologyTest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'session_id',
+        'test_id',
+        'test',
+        'price',
+        'created_by',
+        'payment_status',
+        'results',
+        'tested_by',
+        'status'
+    ];
+}
