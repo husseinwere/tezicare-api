489f6c45e10cdaabd81932b44e88aa518cf418c8
husseinwere <hussein.were@yahoo.com>
2023-08-31 20:09:18 +0300

    nurse instruction

---
+++ app/Http/Controllers/Patient/NurseInstructionController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Patient;
+
+use App\Http\Controllers\Controller;
+use App\Models\Patient\NurseInstruction;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class NurseInstructionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $sessionId = $request->query('session_id');
+
+        $instructions = NurseInstruction::where('session_id', $sessionId)->get();
+
+        return $instructions;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'session_id' => 'required',
+            'instruction' => 'required'
+        ]);
+
+        $data = $request->all();
+        $data['created_by'] = Auth::id();
+
+        $records = explode(';', $data['instruction']);
+        try {
+            foreach ($records as $record) {
+                $data['instruction'] = $record;
+                NurseInstruction::create($data);
+            }
+            return response(null, Response::HTTP_CREATED);
+        }
+        catch (\Exception $e) {
+            return response(['error' => 'An unexpected error has occurred. Please try again'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->all();
+
+        $instruction = NurseInstruction::find($id);
+
+        $updatedInstruction = $instruction->update($data);
+
+        if($updatedInstruction){
+            return response(null, Response::HTTP_OK);
+        }
+        else {
+            return response(['error' => 'An unexpected error has occurred. Please try again'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        if(NurseInstruction::destroy($id)) {
+            return response(null, Response::HTTP_NO_CONTENT);
+        }
+        else {
+            return response(['error' => 'An unexpected error has occurred. Please try again'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
