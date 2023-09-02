9f27fb158ae568852d58f1a4b6e9fca2a340c450
husseinwere <hussein.were@yahoo.com>
2023-09-02 22:59:21 +0300

    payment records

---
+++ app/Models/Billing/PaymentRecord.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Billing;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentRecord extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'session_id',
+        'request_id',
+        'payment_method',
+        'amount',
+        'mpesa_code',
+        'insurance_id',
+        'created_by'
+    ];
+}
---
+++ database/migrations/2023_09_02_192859_create_payment_records_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_records', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('session_id');
+            $table->foreign('session_id')->references('id')->on('patient_sessions');
+            $table->unsignedBigInteger('request_id');
+            $table->foreign('request_id')->references('id')->on('payment_requests');
+            $table->string('payment_method');
+            $table->decimal('amount', 10, 2);
+            $table->string('mpesa_code')->nullable();
+            $table->string('insurance_id')->nullable();
+            $table->foreign('insurance_id')->references('id')->on('patient_insurances');
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_records');
+    }
+};
