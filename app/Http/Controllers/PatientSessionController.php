72cc26ec181508c933c865a089d90c01bfe172fe
Hussein Were <hussein.were@yahoo.com>
2023-09-17 11:46:26 +0300

    latest patient session first

--- app/Http/Controllers/PatientSessionController.php
+++ app/Http/Controllers/PatientSessionController.php
@@ -14,7 +14,7 @@ class PatientSessionController extends Controller
     {
         $patient_id = $request->input('patient_id');
 
-        return PatientSession::where('patient_id', $patient_id)->limit(10)->get();
+        return PatientSession::where('patient_id', $patient_id)->limit(10)->latest()->get();
     }
 
     public function getPatientStats(string $patient_id)
