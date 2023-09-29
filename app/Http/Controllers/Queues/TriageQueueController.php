c60dbe8e7527711141eacf77a24ac821767e9bd9
Hussein Were <hussein.were@yahoo.com>
2023-09-29 18:34:30 +0300

    send patient to doctor from triage

--- app/Http/Controllers/Queues/TriageQueueController.php
+++ app/Http/Controllers/Queues/TriageQueueController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers\Queues;
 
+use App\Models\PatientSession;
+use App\Models\Queues\DoctorQueue;
 use App\Models\Queues\TriageQueue;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class TriageQueueController extends QueueBaseController
 {
     public function __construct(TriageQueue $model)
     {
         parent::__construct($model);
+    }
+
+    public function sendToDoctor(string $session_id) {
+        $session = PatientSession::find($session_id);
+        $data['session_id'] = $session_id;
+
+        if($session['consultation_type'] == "General") {
+            $data['created_by'] = Auth::id();
+
+            $createdItem = DoctorQueue::create($data);
+
+            if($createdItem){
+                $triage = TriageQueue::where('session_id', $session_id)->first();
+                $triage->delete();
+
+                return response(['message' => 'Patient now in doctor queue'], Response::HTTP_CREATED);
+            }
+            else {
+                return response(['message' => 'An unexpected error has occurred. Please try again'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        }
     }
 }
