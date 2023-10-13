2d072963fe283060cc44a34f2cdf7674adcd5f33
Hussein Were <hussein.were@yahoo.com>
2023-10-13 19:32:01 +0600

    lab results model, migration

---
+++ app/Models/Lab/LabResult.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Lab;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LabResult extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'test_id',
+        'result',
+        'description',
+        'file',
+        'created_by'
+    ];
+}
---
+++ database/migrations/2023_10_13_080829_create_lab_results_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lab_results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('test_id');
+            $table->foreign('test_id')->references('id')->on('patient_tests');
+            $table->string('result');
+            $table->string('description')->nullable();
+            $table->string('file')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lab_results');
+    }
+};
