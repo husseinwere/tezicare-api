86f567c981ad67aa376bb530f5db305a212be342
Hussein Were <hussein.were@yahoo.com>
2023-10-14 12:51:50 +0600

    create and edit lab result

--- app/Http/Controllers/Lab/LabResultController.php
+++ app/Http/Controllers/Lab/LabResultController.php
@@ -3,9 +3,50 @@
 namespace App\Http\Controllers\Lab;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lab\LabResult;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class LabResultController extends Controller
 {
-    //
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'test_id' => 'required',
+            'result' => 'required'
+        ]);
+        $data = $request->all();
+        $data['created_by'] = Auth::id();
+
+        $createdResult = LabResult::create($data);
+
+        if($createdResult){
+            return response(null, Response::HTTP_CREATED);
+        }
+        else {
+            return response(['message' => 'An unexpected error has occurred. Please try again'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->all();
+
+        $result = LabResult::find($id);
+        $updatedResult = $result->update($data);
+
+        if($updatedResult){
+            return response(null, Response::HTTP_OK);
+        }
+        else {
+            return response(['message' => 'An unexpected error has occurred. Please try again'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
